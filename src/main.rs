7f7f5a00339ed468c635b0b79f51baf18df86a9e
zhouquan <quan.zhou@kone.com>
2024-07-26 12:51:37 +0800

    misc: fix empty file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io};
 use std::fs;
 
 mod scanner;
@@ -17,13 +17,18 @@ fn main() {
     match command.as_str() {
         "tokenize" => {
             // Read file contents
-            let file_contents = match fs::read_to_string(filename) {
-                Ok(contents) => contents,
-                Err(e) => {
-                    eprintln!("Failed to read file {}: {}", filename, e);
-                    String::new()
-                }
-            };
+            // let file_contents = match fs::read_to_string(filename) {
+            //     Ok(contents) => contents,
+            //     Err(e) => {
+            //         eprintln!("Failed to read file {}: {}", filename, e);
+            //         String::new()
+            //     }
+            // };
+            
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", filename);
+                String::new()
+            });
 
             // Create scanner and process tokens
             if file_contents.is_empty() {
